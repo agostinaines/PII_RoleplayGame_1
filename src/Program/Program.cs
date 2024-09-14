3cff08198352783d8b166284fa4f9ed30ffe35cd
Pilar Antelo <124516059+pilarA27@users.noreply.github.com>
2024-09-13 21:04:23 -0300

    personajesPilar
    Creacion de personajes y acciones

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,21 +8,33 @@ public class Program
     public static void Main()
     {
         // Items
-        
+
+        //Items Luis
         Item martillo = new Item("martillo", 20);
         Item botas = new Item("botas", 5);
         Item lanzaDardos = new Item("lanza dardos", 45);
         Item bastonCurativo = new Item("Baston curativo", 20);
         Item Baculo = new Item("Baculo", 55);
+        //Item Pilar
+        Item varitaMagica = new Item("Varita Magica", 35);
+        Item anilloBosque = new Item("Anillo Del bosque", 12);
+        Item manto = new Item("Manto Potenciador", 10);
+        //Agostina
         
         // Spells
 
+        //Spells Luis
         Spell rayo = new Spell("Rayo electrico", 100);
         Spell fuego = new Spell("Bola de fuego", 90);
-
+        //Spells Pilar
+        Spell rafaga = new Spell("Rafaga de Viento", 80);
+        Spell absorcion = new Spell("Absorcion de vida", 20);
+        Spell tsunami = new Spell("Tsunami de agua", 110);
+        //Spells Agostina
         
         // Creacion de personajes
 
+        //PERSONAJES LUIS
         Enano poppy = new Enano("Poppy", 200);
         poppy.AddItem(martillo);
         poppy.AddItem(botas);
@@ -62,6 +74,36 @@ public class Program
         // Veigar usa un spell contra Poppy
         poppy.RecibirAtaque(veigar.UsarSpell(rayo));
         Console.WriteLine(poppy.Life);
+
+        //PERSONAJES PILAR
+
+        Mago gandalf = new Mago("Gandalf", 300);
+        gandalf.AddItem(varitaMagica);
+        gandalf.AddItem(manto);
+        gandalf.AddSpell(rayo);
+        gandalf.AddSpell(absorcion);
+
+        Elfo elfoNav = new Enano("Elfo Navideño", 80);
+        elfoNav.AddItem(anilloBosque);
+        elfoNav.AddItem(botas);
+        elfoNav.AddItem(lanzaDardos);
+
+        Console.WriteLine("Vida de Gandalf " + gandalf.Life);
+        Console.WriteLine("Valor de ataque de Elfo Navideño" + elfoNav.ValorAtaque);
+
+        // Gandalf ataca a Soraka
+        soraka.RecibirAtaque(gandalf.ValorAtaque);
+        Console.WriteLine(poppy.Life);
+        
+        // Soraka se cura
+        soraka.Curar();
+        Console.WriteLine(soraka.Life);
+
+        // Gandalf usa un spell contra Elfo Navideño
+        elfoNav.RecibirAtaque(gandalf.UsarSpell(absorcion));
+        Console.WriteLine(elfoNav.Life);
+
+        //PERSONAJES AGOSTINA
 
     }
     
