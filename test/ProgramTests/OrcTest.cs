badeac541425830bcd882f09a3a5ed61a4cbd628
Agostina Etchebarren <agostina.etchebarren@correo.ucu.edu.uy>
2024-10-21 21:34:44 -0300

    Giant&Troll Tests

--- test/ProgramTests/OrcTest.cs
+++ test/ProgramTests/OrcTest.cs
@@ -6,47 +6,47 @@ namespace ProgramTests
 {
     public class OrcTest
     {
-        private Orc _orc;
+        private Orc orc;
 
         [SetUp]
         public void Setup()
         {
             // Inicializamos un objeto Orc antes de cada test con un nombre, 150 de vida y 15 VP
-            _orc = new Orc("Gruk", 150, 15);
+            orc = new Orc("Gruk", 150, 15);
         }
 
         [Test]
         public void CrearOrc_ValoresInicialesCorrectos()
         {
             // Verificamos que los valores iniciales son los esperados
-            Assert.That(_orc.Name, Is.EqualTo("Gruk"));
-            Assert.That(_orc.Health, Is.EqualTo(150));
-            Assert.That(_orc.MaxHealth, Is.EqualTo(150));
-            Assert.That(_orc.AttackValue, Is.EqualTo(0));
-            Assert.That(_orc.Items.Count, Is.EqualTo(0));
-            Assert.That(_orc.VictoryPoints, Is.EqualTo(15));
+            Assert.That(orc.Name, Is.EqualTo("Gruk"));
+            Assert.That(orc.Health, Is.EqualTo(150));
+            Assert.That(orc.MaxHealth, Is.EqualTo(150));
+            Assert.That(orc.AttackValue, Is.EqualTo(0));
+            Assert.That(orc.Items.Count, Is.EqualTo(0));
+            Assert.That(orc.VictoryPoints, Is.EqualTo(15));
         }
 
         [Test]
         public void RecibirAtaque_ReduceVida()
         {
-            _orc.ReceiveAttack(30);
-            Assert.That(_orc.Health, Is.EqualTo(120));
+            orc.ReceiveAttack(30);
+            Assert.That(orc.Health, Is.EqualTo(120));
         }
 
         [Test]
         public void RecibirAtaque_NoBajaDeCero()
         {
-            _orc.ReceiveAttack(200);
-            Assert.That(_orc.Health, Is.EqualTo(0));
+            orc.ReceiveAttack(200);
+            Assert.That(orc.Health, Is.EqualTo(0));
         }
 
         [Test]
         public void Curar_RestauraVidaAlMaximo()
         {
-            _orc.ReceiveAttack(50);
-            _orc.Cure(); // Curar restaura la vida al máximo
-            Assert.That(_orc.Health, Is.EqualTo(150));
+            orc.ReceiveAttack(50);
+            orc.Cure(); // Curar restaura la vida al máximo
+            Assert.That(orc.Health, Is.EqualTo(150));
         }
 
         [Test]
@@ -54,22 +54,22 @@ namespace ProgramTests
         {
             // Creamos un item de prueba
             Sword espadaRustica = new Sword("Espada Rustica", 25, 0);           
-            _orc.AddItem(espadaRustica);
+            orc.AddItem(espadaRustica);
 
             // Verificamos que el valor de ataque aumenta
-            Assert.That(_orc.AttackValue, Is.EqualTo(25));
-            Assert.That(_orc.Items.Count, Is.EqualTo(1));
+            Assert.That(orc.AttackValue, Is.EqualTo(25));
+            Assert.That(orc.Items.Count, Is.EqualTo(1));
         }
 
         [Test]
         public void RecibirAtaque_AEnemigoMuerto_NoCambiaVida()
         {
             // Reducimos la vida a cero
-            _orc.ReceiveAttack(200);
-            _orc.ReceiveAttack(30);
+            orc.ReceiveAttack(200);
+            orc.ReceiveAttack(30);
 
             // Verificamos que la vida no cambia
-            Assert.That(_orc.Health, Is.EqualTo(0));
+            Assert.That(orc.Health, Is.EqualTo(0));
         }
     }
 }
